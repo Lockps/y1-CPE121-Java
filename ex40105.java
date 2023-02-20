d3ad75babe99c2f1ea2949841debe31b4b892dc4
Babyshark <Rulic1137@gmail.com>
2023-02-21 05:51:17 +0700

    update

---
+++ ex40105.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ex40105 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int out = reverse(input);
+        System.out.println(out);
+        sc.close();
+    }
+
+    public static int reverse(int input) {
+        int out = 0;
+        int s = 0, para = input;
+        while (para != 0) {
+            para /= 10;
+            s++;
+        }
+        System.out.println(s);
+        for (int i = s - 1; i >= 0; i--) {
+            out += (input % 10) * Math.pow(10, i);
+            input /= 10;
+        }
+
+        return out;
+    }
+}
