d8b3de6e64c44f6daaa66820e243199b2d7863f3
Babyshark <Rulic1137@gmail.com>
2023-03-30 18:10:06 +0700

    701

---
+++ ex701.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex701 {
+    {
+        Scanner sc = new Scanner(System.in);
+        String txt = sc.nextLine();
+        boolean op = check(txt);
+        System.out.println(op);
+        sc.close();
+    }
+
+    public static boolean check(String txt) {
+        if (txt.charAt(1) == 'e') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
