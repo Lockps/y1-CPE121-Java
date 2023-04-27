7e2e215750f34899d9bea38744004b17d035112b
xyz <Rulic1137@gmail.com>
2023-04-28 00:10:15 +0700

    a

--- primenumber.java
+++ primenumber.java
@@ -1,11 +1,11 @@
 public class primenumber {
     public static void main(String[] args) {
-        int num = 0;
+        int num = 1;
         int sum = 0;
-        for (int i = 0; i <= num; i++) {
+        for (int i = 1; i <= num; i++) {
             if (num % i == 0)
                 sum++;
-            if (sum == 2) {
+            if (sum == 1) {
                 System.out.println(num);
             }
             sum = 0;
